50064449e16f48168f789a4daa459be67408f03d
Atul Tameshwari <atameshwari9@gmail.com>
2022-10-05 14:21:49 +0000

    refac: now showing options

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-var readlineSync = require("readline-sync");
+const readlineSync = require("readline-sync");
 const chalk = require('chalk');
 
-var score = 0;
-var userName = readlineSync.question(chalk.blueBright("What is your name? "));
+let score = 0;
+const userName = readlineSync.question(chalk.blueBright("What is your name? "));
 
-console.log("Welcome " + chalk.bold.yellow(userName) + " to an exciting Quiz on "+ chalk.bold.yellowBright("FAMOUS CITIES AND THEIR NICKNAMES!!"));
+console.log("Welcome " + chalk.bold.yellow(userName) + " to a Quiz on " + chalk.bold.yellowBright("FAMOUS CITIES AND THEIR NICKNAMES!!"));
 console.log("----------------------");
 
-var highScore = {
-  name : "Atul",
-  score : 10
+const highScore = {
+  name: "Anupam",
+  score: 5
 }
 
-function ask(nickName, realName) {
-  var userAnswer = readlineSync.question(chalk.blueBright("Q)Which city is known as ") + chalk.yellowBright(nickName)+"\nA)");
+function ask(nickName, realName, options) {
 
-  if(userAnswer.toUpperCase() === realName.toUpperCase()) {
+  console.log(chalk.blueBright("Q) Which city is known as " + chalk.yellowBright(nickName) + "\n" + chalk.yellow('Options: ')))
+  options.forEach(city => console.log(city));
+  
+  const userAnswer = readlineSync.question(chalk.blueBright('Please type your answer : '));
+  
+  if (userAnswer.toUpperCase() === realName.toUpperCase()) {
     console.log(chalk.bold.greenBright("You are right!"));
     score++;
   } else {
@@ -27,44 +31,34 @@ function ask(nickName, realName) {
   console.log("----------------------");
 }
 
-var cities = [ {
-  nickName : "The City of Lakes? ",
-  realName : "Bhopal"
+const cities = [{
+  nickName: "The City of Lakes? ",
+  realName: "Bhopal",
+  options: ['Indore', 'Bhopal', 'Surat', 'Kolkata']
 }, {
-  nickName : "The Diamond City? ",
-  realName : "Surat"
+  nickName: "The Diamond City? ",
+  realName: "Surat",
+  options: ['Surat', 'Jabalpur', 'Ranchi', 'Amritsar']
 }, {
-  nickName : "The Pink City ",
-  realName : "Jaipur"
+  nickName: "The Pink City ",
+  realName: "Jaipur",
+  options: ['Jaipur', 'Allahabad', 'Dhanbad', 'Srinagar']
 }, {
-  nickName : "Silicon Valley of India? ",
-  realName : "Bangalore"
+  nickName: "Silicon Valley of India? ",
+  realName: "Bangalore",
+  options: ['Varanasi', 'Coimbatore', 'Gwalior', 'Bangalore']
 }, {
-  nickName : "The Forbidden City? ",
-  realName : "Beijing"
-}, {
-  nickName : "City of Joy? ",
-  realName : "Kolkata"
-}, {
-  nickName : "The City of Dreams? ",
-  realName : "Mumbai"
-}, {
-  nickName : "The Gambling Town? ",
-  realName : "Las Vegas"
-}, {
-  nickName : "The Sun City? ",
-  realName : "Jodhpur"
-}, {
-  nickName : "Manchester of India? ",
-  realName : "Ahmedabad"
+  nickName: "City of Joy? ",
+  realName: "Kolkata",
+  options: ['Madurai', 'Kolkata', 'Raipur', 'Kota']
 }]
 
-for(var i = 0; i<cities.length; i++ ) {
+for (var i = 0; i < cities.length; i++) {
   var currentCity = cities[i];
-  ask(currentCity.nickName, currentCity.realName);
+  ask(currentCity.nickName, currentCity.realName, currentCity.options);
 }
 
 console.log("YAY! Your final score is :", score);
 
-console.log("The latest recorded highscore is of " + chalk.bold.yellowBright(highScore.name) + " with score " + chalk.bold.yellowBright(highScore.score) + ". \nIf you have beaten this highscore send me a screenshot with your highscore score and I'll update this right away. \n" + chalk.bold.greenBright("Thank You for Playing :)"));
+console.log("The latest recorded high score is of " + chalk.bold.yellowBright(highScore.name) + " with score " + chalk.bold.yellowBright(highScore.score) + ". \nIf you have beaten this high score ping me with your high score score and I'll update this right away. \n" + chalk.bold.greenBright("Thank You for playing :)"));
 
